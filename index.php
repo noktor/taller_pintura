51967238ad52bb90d5623563254a7939f6e92c9f
avilac <albert_vila3@hotmail.com>
2013-11-18 14:01:28 +0100

    afegits fitxers que faltaven i acabat en marxa l'estil (falta fer els onclick)

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<title>Taller de Pintura</title>
+		<link rel="stylesheet/less" type="text/css" href="css/style.less" />
+		<script src="js/less-1.5.0.min.js" type="text/javascript"></script>
+	</head>
+
+	<body>
+		<div id="contenidor">
+			<div id="aleta_frontal_dreta" class="porcio"></div>
+			<div id="retrovisor_dret" class="porcio"/></div>
+			<div id="porta_frontal_dreta" class="porcio"/></div>
+			<div id="porta_anterior_dreta" class="porcio"/></div>
+			<div id="aleta_anterior_dreta" class="porcio"/></div>	
+			<div id="paraxocs_anterior" class="porcio"/></div>
+			<div id="malater" class="porcio"/></div>
+			<div id="aleta_anterior_esquerra" class="porcio"/></div>
+			<div id="porta_anterior_esquerra" class="porcio"/></div>
+			<div id="porta_frontal_esquerra" class="porcio"/></div>
+			<div id="retrovisor_esquerra" class="porcio"/></div>
+			<div id="aleta_frontal_esquerra" class="porcio"/></div>
+			<div id="paraxocs_frontal" class="porcio"/></div>
+			<div id="tapa_frontal" class="porcio"/></div>
+			<div id="sostre" class="porcio"/></div>													
+		</div>
+	</body>
+</html>
